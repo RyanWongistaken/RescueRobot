0b9774b7456968530de3eedc831bd641f66143c7
RyanWong <ryan-wong@shaw.ca>
2020-01-26 13:55:05 -0800

    Mockup implemented for review

--- wisp_gui.py
+++ wisp_gui.py
@@ -17,32 +17,42 @@ class HUD(QWidget):
     def initHUD(self):
         self.layout = QGridLayout()
 
+
         # Creation Widgets
         self.CameraButton = QPushButton("Initiate Link")
+        self.Radar = QLabel()
+        self.HUD = QPixmap('Hudmockup.png')
+        self.OxygenLevels = QLabel('riseup')
+        # self.Oxygen = QPixmap('peanut.png')
+
 
         # Link Widget to method
-        self.CameraButton.clicked.connect(self.vid.start_camera)
+        #self.CameraButton.clicked.connect(self.vid.start_camera)
+        self.vid.start_camera()
+        self.Radar.setPixmap(self.HUD)
+        # self.OxygenLevels.setPixmap(self.Oxygen)
 
-        # Widget Locations
-        self.vid.picture.setAlignment(Qt.AlignCenter)
-        # self.CameraButton.move(800, 800) # move button to coordinates x = 100 & y = 70
 
         # Widget Tooltips
         self.CameraButton.setToolTip('start the webcam')
 
         # Add Widget to Layout
-        self.layout.addWidget(self.vid.picture)
-        self.layout.addWidget(self.CameraButton)
+        self.layout.addWidget(self.vid.picture,0,0)
+        self.layout.addWidget(self.Radar,0 ,0)
+
+
 
         # Display Window
         self.setLayout(self.layout)
+
 
 class Video(QWidget):
     def __init__(self):
         super().__init__()  # Run QWidget init
         self.timer = QTimer(self)
-        self. fps = 30 # Set fps update rate
+        self.fps = 30 # Set fps update rate
         self.picture = QLabel()
+
 
     # Start Camera
     @pyqtSlot()
@@ -58,7 +68,7 @@ class Video(QWidget):
             popUp.exec_()
             return
         self.timer.timeout.connect(self.update)
-        self.timer.start(1000./self.fps) # start once every 41msec
+        self.timer.start(1000. /self.fps) # start once every 41msec
 
     # Draw the new frame to replace the old one
     @pyqtSlot()
@@ -68,6 +78,8 @@ class Video(QWidget):
         img = QImage(frame, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
         pix = QPixmap.fromImage(img)
         self.picture.setPixmap(pix)
+
+
 
 def main():
     app = QApplication([])
